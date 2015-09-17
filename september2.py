38b6de04afcbd8a92671f5bcc486334157000215
cs79 <cloudstrife79@gmail.com>
2015-09-17 10:23:30 -0400

    refactor and annotate logic

--- september2.py
+++ september2.py
@@ -15,37 +15,26 @@ def listproduct(list):
 import itertools
 import collections
 
-# helper function
-def count_max_in_tuple(tup):
-    max_val = max(tup)
-    count = 0
-    for i in range(len(tup)):
-        if tup[i] == max_val:
-            count = count + 1
-
-    return count
-
-
 def get_threes(num):
     facs = factors(num)
-    facs = [n for n in facs if n < 19] * 3
+    facs = [n for n in facs if n < 19] * 3  # full possible set of ages to draw combinations from
     temp = itertools.combinations(facs, 3)
     threes = list(set([cand for cand in temp if listproduct(cand) == num])) # need to sort these
-    threes = set([tuple(sorted(item)) for item in threes])
+    threes = set([tuple(sorted(item)) for item in threes])  # get unique triples
     three_sums = {x: sum(x) for x in threes}
     match_sum = [item for item, count in collections.Counter(three_sums.values()).items() if count > 1]
+    # only exactly one match is valid, else the "knowing the address" logic fails:
     if len(match_sum) == 1:
         match_sum = match_sum[0]
+    # pull out the age triples with (unique) matching sums:
     matches = []
     for key, value in three_sums.iteritems():
         if value == match_sum:
             matches.append(key)
-    top_tuple_counts = [count_max_in_tuple(match) for match in matches]
-    # if 1 is a uniuqe top tuple count, we satisfy the logical condition
-    one_count = 0
-    for i in range(len(top_tuple_counts)):
-        if top_tuple_counts[i] == 1:
-            one_count = one_count + 1
+    # ensure that we have a single "oldest child"
+    top_tuple_counts = [match.count(max(match)) for match in matches]
+    # if 1 is a unique top tuple count, we satisfy the logical condition
+    one_count = top_tuple_counts.count(1)
     if one_count == 1:
         return(True)
     else:
